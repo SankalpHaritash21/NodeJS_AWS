1c0e3ff9c8f53d26b7293653bc4e8cc76318e883
Sankalp <sanklpkaushik21@gmail.com>
2024-09-24 13:36:05 +0530

    Running server on PORT 22

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ http
     res.write("Deploying Node.Js on AWS"); //write a response to the client
     res.end(); //end the response
   })
-  .listen(3000); //the server object listens on port 80
+  .listen(22); //the server object listens on port 80
