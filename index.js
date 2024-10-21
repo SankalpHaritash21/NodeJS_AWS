fcb355ea14dbecf3c59958d19306704017bf0086
Sankalp <sanklpkaushik21@gmail.com>
2024-10-21 06:18:17 +0530

    Creating Server

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var http = require("http");
 
 //create a server object:
 http
-  .createServer((req, res) => {
-    res.write("Deploying Node.Js on AWS"); //write a response to the client
+  .createServer(function (req, res) {
+    res.write("** Welcome to GlobalLogic!!!! **"); //write a response to the client
     res.end(); //end the response
   })
-  .listen(3000); //the server object listens on port 80
+  .listen(80); //the server object listens on port 80
